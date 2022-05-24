1cb971c28f9cb1c9969842089edcb1ec9e143465
KurnakovMaksim <maksk210@gmail.com>
2022-05-24 15:12:09 +0900

    Edit getting file

--- DownloadFile/Controllers/FileController.cs
+++ DownloadFile/Controllers/FileController.cs
@@ -14,18 +14,18 @@ public class FileController : ControllerBase
         _env = env;
     }
 
+    //[HttpGet]
+    //public IActionResult Download()
+    //{
+    //    string filePath = Path.Combine(_env.WebRootPath, "MaksimKurnakovBackendDotNetWebDeveloper.pdf");
+    //    string fileType = "application/pdf";
+    //    return PhysicalFile(filePath, fileType);
+    //}
+
     [HttpGet]
-    public IActionResult Download()
+    public VirtualFileResult GetVirtualFile()
     {
-        try
-        {
-            string filePath = Path.Combine(_env.WebRootPath ?? "~\\wwwroot\\", "MaksimKurnakovBackendDotNetWebDeveloper.pdf");
-            string fileType = "application/pdf";
-            return PhysicalFile(filePath, fileType);
-        }
-        catch (Exception ex)
-        {
-            return Ok(ex.Message + " Stack trace::: " + ex.StackTrace);
-        }
+        var filepath = Path.Combine("~/Files", "MaksimKurnakovBackendDotNetWebDeveloper.pdf");
+        return File(filepath, "text/plain", "MaksimKurnakovBackendDotNetWebDeveloper.pdf");
     }
 }
