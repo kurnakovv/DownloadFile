77a98aa1a98a0a1c477eb4410879f7a48519d578
KurnakovMaksim <maksk210@gmail.com>
2022-05-24 15:20:31 +0900

    Delete useless code

--- DownloadFile/Controllers/FileController.cs
+++ DownloadFile/Controllers/FileController.cs
@@ -6,24 +6,8 @@ namespace DownloadFile.Controllers;
 [ApiController]
 public class FileController : ControllerBase
 {
-    private readonly IWebHostEnvironment _env;
-
-    public FileController(
-        IWebHostEnvironment env)
-    {
-        _env = env;
-    }
-
-    //[HttpGet]
-    //public IActionResult Download()
-    //{
-    //    string filePath = Path.Combine(_env.WebRootPath, "MaksimKurnakovBackendDotNetWebDeveloper.pdf");
-    //    string fileType = "application/pdf";
-    //    return PhysicalFile(filePath, fileType);
-    //}
-
     [HttpGet]
-    public VirtualFileResult GetVirtualFile()
+    public VirtualFileResult Download()
     {
         var filepath = Path.Combine("~/Files", "MaksimKurnakovBackendDotNetWebDeveloper.pdf");
         return File(filepath, "text/plain", "MaksimKurnakovBackendDotNetWebDeveloper.pdf");
