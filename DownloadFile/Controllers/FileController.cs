6370301892848e8b5b340dffc19e008c8a22bce1
KurnakovMaksim <maksk210@gmail.com>
2022-05-24 14:12:21 +0900

    Add project files.

---
+++ DownloadFile/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DownloadFile.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FileController : ControllerBase
+{
+    private readonly IWebHostEnvironment _env;
+
+    public FileController(
+        IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+    [HttpGet]
+    public IActionResult Download()
+    {
+        string filePath = Path.Combine(_env.WebRootPath, "MaksimKurnakovBackendDotNetWebDeveloper.pdf");
+        string fileType = "application/pdf";
+        return PhysicalFile(filePath, fileType);
+    }
+}
